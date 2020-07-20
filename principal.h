2f64fc485af325c0ec8dc8bd06c3931442a89aa7
unknown <you@example.com>
2020-07-19 20:32:07 -0500

    version 0.1

---
+++ principal.h
@@ -0,0 +1,50 @@
+#ifndef PRINCIPAL_H
+#define PRINCIPAL_H
+
+#include <QMainWindow>
+
+#include "producto.h"
+
+#include "notaventa.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class Principal; }
+QT_END_NAMESPACE
+
+class Principal : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    Principal(QWidget *parent = nullptr);
+    ~Principal();
+    QString acum;
+
+public slots:
+    void mostrarPrecio(int);  //Mostrara el precio
+    void agregarProducto();   //Agrega los productos
+    void cedula();            //Validar cedula
+    void terminado();         //
+    void email();          //Validar email
+    void cliente();        //Validar el conmre del cliente
+    void telefono();       //Validar para el ingreso del numero de telefono
+    void imprimirIni();
+    void imprimirInfo();
+    void consumidor();
+    void notaVenta();
+
+private:
+    Ui::Principal *ui;
+    QList<Producto*> m_productos;
+
+    void inicializarDatos();
+    void inicializarWidgets();
+    float m_subtotal;
+
+    void calcular(float subtotal);
+    // este metodo es para agrupar los mismos productos que ya hemos ingresado
+    bool registrar(Producto *producto, int cantidad);
+
+
+};
+#endif // PRINCIPAL_H
